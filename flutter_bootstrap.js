ae4a2a71947b0b68c3882c3d40f106fd1f1cd2f8
Patrick Castle <patrick.c.castle@gmail.com>
2024-07-07 16:09:41 +1000

    Corrected manifest

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4256913691"
+    serviceWorkerVersion: "1211041208"
   }
 });
